58eab35b73ee3602ad5a2289178a1e6e8fd1d71c
mrp4sten <mauricio.pasten.martinez@gmail.com>
2023-12-27 15:59:46 -0600

    Add HTTP server and update package.json

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const _ = require('lodash')
-const numbers = [33, 46, 76, 44, 12, 3]
-_.each(numbers, function (number, i) {
-  console.log(number)
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/html')
+  res.end('<h1>Hello World</h1>')
+})
+
+const port = 3000
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`)
 })
